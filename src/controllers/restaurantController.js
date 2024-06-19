c14f66a8c0e21c2b813646381085e3be6c681e8b
ThaiHung <thaihung147741@gmail.com>
2024-06-19 16:40:10 +0700

    commit

--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -1,7 +1,7 @@
 import initModels from "../models/init-models.js";
 import sequelize from "../models/connect.js";
 import { responseSend } from "../config/response.js";
-import { changeFormatDate, getLikes, getRates } from "../ultils/index.js";
+import { getLike, getLikes, getRate, getRates, processLikes, processRates } from "../utils/index.js";
 
 let model = initModels(sequelize);
 
@@ -27,35 +27,7 @@ const getListUserLike = async (req, res) => {
 
     console.log(JSON.stringify(likes, null, 2));
 
-    const listUserLikeRes = {};
-
-    likes.forEach((like) => {
-      const userId = like.user_id;
-      const userName = like.user.full_name;
-      const userEmail = like.user.email;
-      const restaurantName = like.re.res_name;
-      const date = changeFormatDate(like.date_like);
-
-      if (!listUserLikeRes[userId]) {
-        listUserLikeRes[userId] = {
-          name: userName,
-          email: userEmail,
-          listResLike: [],
-        };
-      }
-
-      listUserLikeRes[userId].listResLike.push({
-        resName: restaurantName,
-        date,
-      });
-    });
-
-    const result = Object.keys(listUserLikeRes).map((userId) => {
-      return {
-        id: parseInt(userId),
-        ...listUserLikeRes[userId],
-      };
-    });
+    let result = processLikes(likes, 'user')
     responseSend(res, result, "Successfully!", 200);
   } catch (error) {
     responseSend(res, "", error.message, 500);
@@ -69,28 +41,7 @@ const getUserLike = async (req, res) => {
 
     console.log(JSON.stringify(likes, null, 2));
 
-    if (!likes.length) {
-      return responseSend(
-        res,
-        "",
-        "User not found or no likes for this user!",
-        404
-      );
-    }
-
-    const userName = likes[0].user.full_name;
-    const userEmail = likes[0].user.email;
-    const listLike = likes.map((like) => ({
-      name: like.re.res_name,
-      dateLike: changeFormatDate(like.date_like),
-    }));
-
-    const result = {
-      name: userName,
-      email: userEmail,
-      listLike,
-    };
-
+    let result = getLike(likes, "user")
     responseSend(res, result, "Successfully!", 200);
   } catch (err) {
     responseSend(res, "", err.message, 500);
@@ -101,36 +52,7 @@ const getListRestaurantLike = async (req, res) => {
   try {
     let likes = await getLikes();
 
-    let listRestaurantsLike = [];
-
-    likes.forEach((like) => {
-      const resId = like.res_id;
-      const userId = like.user_id;
-      const userName = like.user.full_name;
-      const email = like.user.email;
-      const restaurantName = like.re.res_name;
-
-      if (!listRestaurantsLike[resId]) {
-        listRestaurantsLike[resId] = {
-          name: restaurantName,
-          listUserLike: [],
-        };
-      }
-
-      const infoUser = {
-        userId,
-        userName,
-        email,
-        dateLike: changeFormatDate(like.date_like),
-      };
-
-      listRestaurantsLike[resId].listUserLike.push(infoUser);
-    });
-
-    let result = Object.keys(listRestaurantsLike).map((resId) => ({
-      id: parseInt(resId),
-      ...listRestaurantsLike[resId],
-    }));
+    let result = processLikes(likes, 'restaurant')
 
     responseSend(res, result, "Successfully", 200);
   } catch (err) {
@@ -144,26 +66,7 @@ const getRestaurantLike = async (req, res) => {
   try {
     const likes = await getLikes({ res_id });
 
-    if (!likes.length) {
-      return responseSend(
-        res,
-        "",
-        "Restaurant not found or no likes for this restaurant!",
-        404
-      );
-    }
-    console.log(JSON.stringify(likes, null, 2));
-    const restaurantName = likes[0].re.res_name;
-    const listUserLike = likes.map((like) => ({
-      userId: like.user_id,
-      userName: like.user.full_name,
-      dateLike: changeFormatDate(like.date_like),
-    }));
-
-    const result = {
-      name: restaurantName,
-      listUserLike,
-    };
+    let result = getLike(likes, "restaurant")
     responseSend(res, result, "Successfully!", 200);
   } catch (err) {
     responseSend(res, "", err.message, 500);
@@ -173,7 +76,7 @@ const getRestaurantLike = async (req, res) => {
 const addRateRestaurant = async (req, res) => {
   const { user_id, res_id, amount } = req.body;
   try {
-    const rate = await model.rate_res.create({
+    await model.rate_res.create({
       user_id,
       res_id,
       amount,
@@ -192,40 +95,7 @@ const getListRateRestaurant = async (req, res) => {
 
     console.log(JSON.stringify(rates, null, 2));
 
-    const listRateRes = {};
-
-    rates.forEach((rate) => {
-      const resId = rate.res_id;
-      const userId = rate.user_id;
-      const userName = rate.user.full_name;
-      const restaurantName = rate.re.res_name;
-      const amount = rate.amount;
-
-      if (!listRateRes[resId]) {
-        listRateRes[resId] = {
-          name: restaurantName,
-          listUserRate: [],
-        };
-      }
-      const userRate = listRateRes[resId].listUserRate.find(
-        (user) => user.userId === userId
-      );
-      if (userRate) {
-        userRate.amount += amount;
-      } else {
-        listRateRes[resId].listUserRate.push({
-          userId,
-          userName,
-          amount,
-          dateRate: changeFormatDate(rate.date_rate),
-        });
-      }
-    });
-
-    const result = Object.keys(listRateRes).map((resId) => ({
-      id: parseInt(resId),
-      ...listRateRes[resId],
-    }));
+    let result = processRates(rates, "restaurant")
     responseSend(res, result, "Successfully!", 200);
   } catch (err) {
     responseSend(res, "", err.message, 500);
@@ -238,28 +108,7 @@ const getRateRestaurant = async (req, res) => {
     const ratings = await getRates({ res_id });
     console.log(JSON.stringify(ratings, null, 2));
 
-    if (!ratings.length) {
-      return responseSend(
-        res,
-        "",
-        "Restaurant not found or no ratings for this restaurant!",
-        404
-      );
-    }
-
-    const restaurantName = ratings[0].re.res_name;
-
-    const listUserRate = ratings.map((rating) => ({
-      userId: rating.user_id,
-      name: rating.user.full_name,
-      email: rating.user.email,
-      dateRate: changeFormatDate(rating.date_rate),
-    }));
-
-    const result = {
-      name: restaurantName,
-      listUserRate,
-    };
+    let result = getRate(ratings, "restaurant")
 
     responseSend(res, result, "Successfully!", 200);
   } catch (err) {
@@ -273,40 +122,7 @@ const getListRateUser = async (req, res) => {
 
     console.log(JSON.stringify(rates, null, 2));
 
-    const listRateUser = {};
-
-    rates.forEach((rate) => {
-      const resId = rate.res_id;
-      const userId = rate.user_id;
-      const userName = rate.user.full_name;
-      const restaurantName = rate.re.res_name;
-      const amount = rate.amount;
-
-      if (!listRateUser[userId]) {
-        listRateUser[userId] = {
-          name: userName,
-          listResRate: [],
-        };
-      }
-      const resRate = listRateUser[userId].listResRate.find(
-        (res) => res.resId === resId
-      );
-      if (resRate) {
-        resRate.amount += amount;
-      } else {
-        listRateUser[userId].listResRate.push({
-          resId,
-          restaurantName,
-          amount,
-          dateRate: changeFormatDate(rate.date_rate)
-        });
-      }
-    });
-
-    const result = Object.keys(listRateUser).map((userId) => ({
-      id: parseInt(userId),
-      ...listRateUser[userId],
-    }));
+    let result = processRates(rates, "user")
     responseSend(res, result, "Successfully!", 200);
   } catch (err) {
     responseSend(res, "", err.message, 500);
@@ -320,40 +136,7 @@ const getRateUser = async (req, res) => {
 
     console.log(JSON.stringify(ratings, null, 2));
 
-    if (!ratings.length) {
-      return responseSend(
-        res,
-        "",
-        "User not found or no ratings for this user!",
-        404
-      );
-    }
-
-    const name = ratings[0].user.full_name;
-    const email = ratings[0].user.email;
-    let total = 0;
-    const listResRate = {};
-    ratings.map((rating) => {
-      const resId = rating.res_id;
-
-      if (!listResRate[resId]) {
-        listResRate[resId] = {
-          resName: rating.re.res_name,
-          dateRate: changeFormatDate(rating.date_rate),
-          amount: (total += rating.amount),
-        };
-      }
-    });
-
-    const listRate = Object.keys(listResRate).map((resId) => ({
-      resId: parseInt(resId),
-      ...listResRate[resId],
-    }));
-    const result = {
-      name,
-      email,
-      listRate,
-    };
+    let result = getRate(ratings, "user")
 
     responseSend(res, result, "Successfully!", 200);
   } catch (err) {
---
+++ src/utils/index.js
@@ -0,0 +1,283 @@
+import initModels from "../models/init-models.js";
+import sequelize from "../models/connect.js";
+
+let model = initModels(sequelize);
+
+const getLikes = async (conditions = {}) => {
+  return await model.like_res.findAll({
+    where: conditions,
+    include: [
+      {
+        model: model.restaurant,
+        as: "re",
+        attributes: ["res_name"],
+      },
+      {
+        model: model.user,
+        as: "user",
+        attributes: ["full_name", "email"],
+      },
+    ],
+  });
+};
+
+const getRates = async (conditions = {}) => {
+  return await model.rate_res.findAll({
+    where: conditions,
+    include: [
+      {
+        model: model.restaurant,
+        as: "re",
+        attributes: ["res_name"],
+      },
+      {
+        model: model.user,
+        as: "user",
+        attributes: ["full_name", "email"],
+      },
+    ],
+  });
+};
+
+const objectToArray = (obj) => {
+  return Object.keys(obj).map((key) => ({
+    id: parseInt(key),
+    ...obj[key],
+  }));
+};
+
+const processLikes = (likes, type) => {
+  const object = {};
+
+  likes.forEach((like) => {
+    const resId = like.res_id;
+    const userId = like.user_id;
+    const userName = like.user.full_name;
+    const email = like.user.email;
+    const restaurantName = like.re.res_name;
+    const date = changeFormatDate(like.date_like);
+
+    switch (type) {
+      case "user":
+        if (!object[userId]) {
+          object[userId] = {
+            name: userName,
+            email,
+            listResLike: [],
+          };
+        }
+
+        object[userId].listResLike.push({
+          resName: restaurantName,
+          date,
+        });
+        break;
+      default:
+        if (!object[resId]) {
+          object[resId] = {
+            name: restaurantName,
+            listUserLike: [],
+          };
+        }
+
+        const infoUser = {
+          userId,
+          userName,
+          email,
+          dateLike: date,
+        };
+
+        object[resId].listUserLike.push(infoUser);
+    }
+  });
+
+  return objectToArray(object);
+};
+
+const getLike = (likes, type) => {
+  if (!likes.length) {
+    return res
+      .status(400)
+      .send(`${type} not found or no likes for this ${type}!`);
+  }
+
+  let result = {};
+  switch (type) {
+    case "user":
+      const userName = likes[0].user.full_name;
+      const email = likes[0].user.email;
+      const listLike = likes.map((like) => ({
+        name: like.re.res_name,
+        dateLike: changeFormatDate(like.date_like),
+      }));
+
+      result = {
+        name: userName,
+        email: email,
+        listLike,
+      };
+      break;
+    default:
+      const restaurantName = likes[0].re.res_name;
+      const listUserLike = likes.map((like) => ({
+        userId: like.user_id,
+        userName: like.user.full_name,
+        email: like.user.email,
+        dateLike: changeFormatDate(like.date_like),
+      }));
+
+      result = {
+        name: restaurantName,
+        listUserLike,
+      };
+  }
+  return result;
+};
+
+const processRates = (rates, type) => {
+  const object = {};
+
+  rates.forEach((rate) => {
+    const resId = rate.res_id;
+    const userId = rate.user_id;
+    const userName = rate.user.full_name;
+    const email = rate.user.email;
+    const restaurantName = rate.re.res_name;
+    const amount = rate.amount;
+    const date = changeFormatDate(rate.date_rate);
+
+    switch (type) {
+      case "restaurant":
+        if (!object[resId]) {
+          object[resId] = {
+            name: restaurantName,
+            listUserRate: [],
+          };
+        }
+
+        const userRate = object[resId].listUserRate.find(
+          (user) => user.userId === userId
+        );
+
+        if (userRate) {
+          userRate.amount += amount;
+        } else {
+          object[resId].listUserRate.push({
+            userId,
+            userName,
+            email,
+            amount,
+            dateRate: date,
+          });
+        }
+        break;
+      default:
+        if (!object[userId]) {
+          object[userId] = {
+            name: userName,
+            email,
+            listResRate: [],
+          };
+        }
+        const resRate = object[userId].listResRate.find(
+          (res) => res.resId === resId
+        );
+        if (resRate) {
+          resRate.amount += amount;
+        } else {
+          object[userId].listResRate.push({
+            resId,
+            restaurantName,
+            amount,
+            dateRate: date,
+          });
+        }
+    }
+  });
+
+  return objectToArray(object);
+};
+
+const getRate = (ratings, type) => {
+  if (!ratings.length) {
+    return res
+      .status(400)
+      .send(`${type} not found or no ratings for this ${type}!`);
+  }
+  let result = {};
+  let list = [];
+  const name = ratings[0].user.full_name;
+  const email = ratings[0].user.email;
+  const restaurantName = ratings[0].re.res_name;
+  ratings.forEach((rating) => {
+    const amount = rating.amount;
+    const date = changeFormatDate(rating.date_rate);
+    switch (type) {
+      case "user":
+        const resId = rating.res_id;
+        const resRate = list.find((res) => res.resId === resId);
+        if (!resRate) {
+          list.push({
+            resId,
+            resName: rating.re.res_name,
+            amount,
+            dateRate: date,
+          });
+        } else {
+          resRate.amount += amount;
+        }
+
+        result = {
+          name,
+          email,
+          listRestaurantRate: list,
+        };
+        break;
+      default:
+        const userId = rating.user_id;
+        const userRate = list.find((user) => user.userId === userId);
+        if (!userRate) {
+          list.push({
+            userId,
+            name: rating.user.full_name,
+            email: rating.user.email,
+            amount,
+            dateRate: date,
+          });
+        } else {
+          userRate.amount += amount;
+        }
+
+        result = {
+          name: restaurantName,
+          listUserRate: list,
+        };
+    }
+  });
+
+  return result;
+};
+
+const changeFormatDate = (value) => {
+  const date = new Date(value);
+
+  const pad = (num) => String(num).padStart(2, "0");
+
+  const formattedDate = `${date.getUTCFullYear()}-${pad(
+    date.getUTCMonth() + 1
+  )}-${pad(date.getUTCDate())} ${pad(date.getUTCHours())}:${pad(
+    date.getUTCMinutes()
+  )}:${pad(date.getUTCSeconds())}`;
+
+  return formattedDate;
+};
+
+export {
+  getLikes,
+  getRates,
+  changeFormatDate,
+  processLikes,
+  processRates,
+  getLike,
+  getRate,
+};
